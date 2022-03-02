ee0d162a40fa99c1842fda51ad58854ed71213b5
Austin Clements <austinclementsbass@gmail.com>
2022-03-02 12:56:27 -0600

    finish first assignment

---
+++ src/Example8.java
@@ -0,0 +1,12 @@
+public class Example8 {
+    public static void main(String[] args) {
+
+        int value = 0;
+
+        while(value < 10) {
+            System.out.println("Hello " + value);
+
+            value = value + 1;
+        }
+    }
+}
